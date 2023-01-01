f358486ed401990bb3c40d7de514d1c513b735bd
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-12-31 18:28:52 -0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+
+export default function Home() {
+    return (
+        <section>
+
+            <Navbar />
+            <h2>Design</h2>
+            <h3>Develop & Deploy</h3>
+            <p>UX designer & web developer based in Dallas TX</p>
+        </section>
+    )
+}
